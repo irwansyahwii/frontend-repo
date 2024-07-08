359b0a08436d9910a275844c994add0200674699
Irwansyah <irwansyah@gmail.com>
2024-07-08 20:20:30 +0700

    chore(login): check is still logged in using firebase on the server side

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import LoginForm from "@/components/login.form";
 import { getCurrentUser } from "@/apis/server-side/getCurrentUser";
 import { redirect } from "next/navigation";
+import { isStillLoggedIn } from "@/apis/server-side/isStillLoggedIn";
 
 export default async function Home({params}: {params: string}) {
-  const currentUser = await getCurrentUser();
-  if(!currentUser){
+  const isLoggedIn = await isStillLoggedIn();
+  if(!isLoggedIn){
     redirect('/login');
   }else{
     redirect('/dashboard');
