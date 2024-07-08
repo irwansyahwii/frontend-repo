b87d55a4b22a89e6f2eafc723fbd5977656d06fe
Irwansyah <irwansyah@gmail.com>
2024-07-08 15:27:21 +0700

    chore(login): add snackbar after successful login

--- components/login.form.tsx
+++ components/login.form.tsx
@@ -10,9 +10,14 @@ import Button from '@mui/joy/Button';
 import { RootModel } from "@/store/models";
 import { RematchDispatch } from "@rematch/core";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useTheme } from '@mui/joy/styles';
 import { ThemeSpec } from "@/theme/theme.spec";
+import Snackbar from '@mui/joy/Snackbar';
+import { delay } from "@/utils/delay";
+
+
+
 
 
 
@@ -23,37 +28,43 @@ const mapState = (state: RootState) => ({
 
 
 const LoginForm = (props: any) => {
-  
+  const [snackBarMessage, setSnackBarMessage] = useState<string>("");
   const theme: ThemeSpec = useTheme() as any;
   
   const {auth, dispatch, loading}:{loading: boolean, auth: AuthState, dispatch: RematchDispatch<RootModel>} = props;
 
   const router = useRouter();
   const doSignIn = async ()=>{
-    await dispatch.auth.loginWithGoogle();
-
-    
+    await dispatch.auth.loginWithGoogle();    
   }
+
+  const navigateToDashboard = useCallback(async ()=>{
+    await delay(800);
+    router.replace('/dashboard');
+  },[router]);
 
   useEffect(()=>{
     if(auth.isLoggedIn){
-      router.replace('/dashboard');
+      setSnackBarMessage("You have successfully logged in!");
+      navigateToDashboard();
     }
-  }, [router, auth.isLoggedIn]);
+  }, [router, auth.isLoggedIn, navigateToDashboard]);
   return (
-    
-    <Sheet
-      sx={theme.loginForm.sheet}
-      variant={theme.loginForm.sheet.variant}
-    >
-      <div>
-        <Typography level={theme.loginForm.caption.heading} component={theme.loginForm.caption.heading}>
-          <b>Welcome!</b>
-        </Typography>
-        <Typography level={theme.loginForm.instruction.heading}>Sign in to continue.</Typography>
-      </div>
-      <Button loading={loading} onClick={doSignIn} variant={theme.loginForm.signInButton.variant} sx={theme.loginForm.signInButton}>Sign in with Google</Button>
-    </Sheet>      
+    <>
+      <Snackbar anchorOrigin={{vertical: 'top', horizontal: 'center'}} open={snackBarMessage.length > 0} >{snackBarMessage}</Snackbar>
+      <Sheet
+        sx={theme.loginForm.sheet}
+        variant={theme.loginForm.sheet.variant}
+      >
+        <div>
+          <Typography level={theme.loginForm.caption.heading} component={theme.loginForm.caption.heading}>
+            <b>Welcome!</b>
+          </Typography>
+          <Typography level={theme.loginForm.instruction.heading}>Sign in to continue.</Typography>
+        </div>
+        <Button loading={loading} onClick={doSignIn} variant={theme.loginForm.signInButton.variant} sx={theme.loginForm.signInButton}>Sign in with Google</Button>
+      </Sheet>      
+    </>
 
   )
 }
